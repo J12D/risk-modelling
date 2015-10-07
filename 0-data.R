c4888e60189305c65400a8ae88eaf53ff097412d
J12D <heinzderalligator@googlemail.com>
2015-10-07 19:32:40 +0200

    Fix data processing

--- 0-data.R
+++ 0-data.R
@@ -9,15 +9,12 @@ Sys.setenv(tz = "UTC")
 price_ <- read_excel("SwissAirAktie.xls", skip = 1, na = "#N/A N/A",col_types = c("date","numeric","numeric","numeric")) %>% as.data.frame
 price_ <- xts(price_[,-1], as.Date(price_[,1]))
 colnames(price_) <- c("Last", "Last_Actual", "Mid")
-price <- price$Last
+price <- price_$Last
 
-returns <- price %>%
-           xts2df %>%
-           transmute(time = .[,"time"],
-                     continuous = ROC(.),
-                     discrete = ROC(., type = "discrete")) %>%
-           na.omit %>%
-           df2xts
+returns <- xts(NULL)
+returns$continuous <- ROC(price)
+returns$discrete <- ROC(price, type = "discrete")
+returns %<>% na.omit
            
 price$Last %>% chartSeries
 returns$discrete %>% chartSeries
