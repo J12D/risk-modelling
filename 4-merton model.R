a8fa3da28b200e7465cd0a8ef92ba5ffc0350c46
J12D <heinzderalligator@googlemail.com>
2015-10-17 14:05:56 +0200

    Changes to merton model

--- 4-merton model.R
+++ 4-merton model.R
@@ -1,19 +1,32 @@
 ##--------Swissair Equity----------------------------------------
-SA_BS <- read_excel("data/Equity-Swissair.xlsx") %>% as.data.frame
-SAequity <- xts(SA_BS[,4], SAequity[,1] %>% parse_date_time("%Y/%m/%d"))
-SAdebt <- xts(SA_BS[,5], SAequity[,1] %>% parse_date_time("%Y/%m/%d"))
-SAasset<- xts(SA_BS[,6], SAequity[,1] %>% parse_date_time("%Y/%m/%d"))
-
+SA_BS <- read_excel("data/Equity-Swissair.xlsx", na = "#N/A N/A")
+SA_BS <- xts(SA_BS[,2:6], SA_BS[,1][[1]] %>% as.Date)
+colnames(SA_BS) <- c("price", "shares", "equity", "debt", "assets")
 ##--------Set risk_free rate and default period------------------
 t <- 1
 r <- 0.05
 
-##--------Import volatilities from GARCH model-------------------
-vol_garch <- 
+## ---- Get volatilities from GARCH model-------------------
+garch_vol <- function(q=99.9E-2) {
+  function(prices) {
+    returns <- prices %>% ROC(type = "discrete") %>% na.omit
+    res <- garchFit(~garch(1,1), data = returns, trace = F)
+    forecast <- predict(res, 252)
+    sigma <- forecast$standardDeviation ^ 2 %>% sum %>% sqrt(.)
+    # forecast <- predice(res, 1)
+    # sigma <- forecast$standardDeviation
+    sigma
+  }
+}
 
+
+vol_garch <- evaluate_model(SA_BS$price, garch_vol(99E-2), "4 quarters", 200)
+vol_garch %>% plotXTS
+
+aggr <- merge.xts(SA_BS[,c("assets", "debt")], vol_garch) %>% na.omit
 
 ##--------get the probability------------------------------------
-d2 <- log(SAasset/SAequity) + r*t - (vol_garch)^2*t
-PD <- 1- qnorm(d2)
+d2 <- (log(aggr$assets / aggr$debt) + r * t - (aggr$vol_garch) ^ 2 * t * 0.5)/(aggr$vol_garch * sqrt(t))
+PD <- 1 - pnorm(d2)
 
-
+PD %>% plotTable("plot-data/pd")
